3ab85d673948b80dba256a5b371ef3bd5eeaa927
srandy <sandeepranjan2007@yahoo.co.in>
2018-07-21 22:23:53 +0530

    Chapter 2 completed

---
+++ Chapter-2/highArrayApp.java
@@ -0,0 +1,161 @@
+/*
+	Solution for Programming Projects
+	2.1, 2.2, 2.3, 2.6
+*/
+
+
+import java.util.*;
+
+class HighArray {
+	private int[] a;
+	private int nEle;
+	
+	public HighArray(int size) {
+		a = new int[size];
+		nEle = 0;
+	}
+
+	public int getNumEle() {
+		return nEle;
+	}
+ 
+	public void display() {
+		for(int i=0; i<nEle; i++) 
+			System.out.print(a[i] + " ");
+
+		System.out.println();
+	}
+
+	public void insert(int val) {
+		a[nEle] = val;
+		nEle++;
+	}
+
+	public int search(int val) {
+		int i;
+		for(i=0; i<nEle; i++) 
+			if(a[i] == val) break; 
+
+		if(i == nEle) return (-1);
+		else return i;
+	}
+
+	public int delete(int val) {
+		int index = search(val);
+		if(index != -1) {
+			for(int k=index; k<nEle-1; k++) { 
+				a[k] = a[k+1];
+			}
+			nEle--;
+			return 1;
+		}else {
+			return 0;
+		}	
+	}
+
+	public int getMax() {
+		if(nEle == 0) return -1;
+
+		int max = a[0];
+		for(int i=1; i<nEle; i++) {
+			if(a[i] > max) max = a[i];	
+		}
+		return max;
+	}
+
+	public int removeMax() {
+		int max = getMax();
+		delete(max);
+		return max;
+	}	
+
+	public void noDups() {
+		int value = 0;
+
+		for(int i=0; i<nEle; i++) {
+			value = a[i];
+
+			for(int j=i+1; j<nEle; j++) {
+
+				if(a[j] == value) {
+					for(int k=j; k<nEle-1; k++) {
+						a[k] = a[k+1];
+					}
+					nEle--;
+					j--;
+					size--;
+				}
+			}
+		}
+	}
+	
+}
+
+
+class highArrayApp {
+	public static void main(String[] args) {
+		int size = 20;
+		HighArray ar = new HighArray(size);
+		Scanner sc = new Scanner(System.in);
+	
+		ar.insert(34);
+		ar.insert(56);
+		ar.insert(23);
+		ar.insert(12);
+		ar.insert(90);
+		ar.insert(89);
+		ar.insert(21);
+		ar.insert(77); 
+		ar.insert(99); 
+		ar.insert(44); 
+		ar.insert(55); 
+		ar.insert(22); 
+		ar.insert(88); 
+		ar.insert(11); 
+		ar.insert(00); 
+		ar.insert(66); 
+		ar.insert(33);
+
+		ar.display();
+		
+		// ELEMENT TO BE SEARCHED
+		/*
+		System.out.println("Enter the element to be seached");
+		int sarch = sc.nextInt();
+
+		if(ar.search(sarch) != -1) {
+			System.out.println("Element found");
+		} else {
+			System.out.println("Element not found");
+		}
+		*/
+
+		// ELEMENT TO BE DELETED
+		/*
+		System.out.println("Enter the element to be deleted");
+            int del = sc.nextInt();
+
+		ar.delete(del);
+		*/
+
+		// SORT ARRAY IN DESC ORDER
+		/*
+		HighArray ar2 = new HighArray(size);
+		
+		int len = ar.getNumEle();
+
+		for(int i=0; i<len; i++) {
+			int ele = ar.removeMax();
+			ar2.insert(ele);
+		}
+
+		ar2.display();
+		*/
+
+		// REMOVE DUPLICATES
+		/*
+		ar.noDups();
+		ar.display();
+		*/
+	}
+}
---
+++ Chapter-2/orderedArray.java
@@ -0,0 +1,190 @@
+/*
+	Solution for Programming Projects
+	2.4, 2.5
+*/
+
+class OrdArray {
+	private int[] a;
+	private int nElems; 
+
+	public OrdArray(int max) {
+		a = new int[max];
+	}
+
+	public int size() { 
+		return nElems; 
+	}
+
+	public int getElementFromPos(int index) {
+		return a[index];
+	}
+
+	public void setElementAtPos(int index, int val) {
+		a[index] = val;
+	}
+
+	public int find(int searchKey) {		
+		int lowerBound = 0;
+		int upperBound = nElems-1; 
+		int curIn;
+
+		while(true) {
+			curIn = (lowerBound + upperBound) / 2;
+
+			if(a[curIn]==searchKey)
+				return curIn; 
+			else if(lowerBound > upperBound) 
+				return nElems;  
+			else {
+				if(a[curIn] < searchKey)
+					lowerBound = curIn + 1;
+				else
+					upperBound = curIn - 1;  
+				} 
+		} 
+	}
+
+	public void insert(int value) {
+		int lowerBound = 0;
+		int upperBound = nElems - 1;
+		int j = 0;
+
+		while(true) {
+
+			if(lowerBound > upperBound) break;
+			j = (lowerBound + upperBound) / 2;
+
+			if(value > a[j]) {
+				lowerBound = j + 1;
+				j++;
+			}
+			else upperBound = j - 1;
+		}
+
+		for(int k=nElems; k>j; k--) 
+			a[k] = a[k-1];
+
+		a[j] = value;
+		nElems++; 
+	} 
+
+	public boolean delete(int value) {
+		int j = find(value);
+
+		if(j==nElems) 
+			return false;
+		else {
+			for(int k=j; k<nElems; k++) 
+				a[k] = a[k+1];
+			nElems--; 
+			return true;
+		}
+	} 
+	 
+	public void display() {
+		for(int j=0; j<nElems; j++) 
+			System.out.print(a[j] + " "); 
+		System.out.println("");
+	}
+
+	public static OrdArray merge(OrdArray a, OrdArray b, int size) {
+
+		OrdArray c = new OrdArray(size);
+		
+		int j = 0, k = 0;
+
+		while(j < a.size() && k < b.size()) {
+
+			if(a.getElementFromPos(j) <= b.getElementFromPos(k)) {
+				c.setElementAtPos(c.nElems, a.getElementFromPos(j));
+				j++;
+			}
+			else {
+				c.setElementAtPos(c.nElems, b.getElementFromPos(k));
+				k++;
+			}
+
+			c.nElems++;
+		}
+
+		while(j < a.size())
+		{
+			c.setElementAtPos(c.nElems, a.getElementFromPos(j));
+			c.nElems++;
+			j++;
+		}
+		
+		while(k < b.size())
+		{
+			c.setElementAtPos(c.nElems, b.getElementFromPos(k));
+			c.nElems++;
+			k++;
+		}
+		return c;
+
+	}
+}	 
+
+
+class orderedArray {
+
+	public static void main(String[] args) {
+		int maxSize = 100; 
+		OrdArray arr = new OrdArray(maxSize);
+
+		arr.insert(77); 
+		arr.insert(99); 
+		arr.insert(44); 
+		arr.insert(55); 
+		arr.insert(22); 
+		arr.insert(88); 
+		arr.insert(11); 
+		arr.insert(12); 
+		arr.insert(00); 
+		arr.insert(66); 
+		arr.insert(33); 
+
+		arr.display();
+
+		//ELEMENT TO BE SEARCHED
+		/*
+		int searchKey = 11;
+
+		if( arr.find(searchKey) != arr.size())
+			System.out.println("Found " + searchKey); 
+		else
+			System.out.println("Can’t find " + searchKey);
+
+		arr.display();
+		*/
+
+		//ELEMENT TO BE DELETED
+		/*
+		arr.delete(00); 
+		arr.delete(55); 
+		arr.delete(99);
+		*/
+
+		OrdArray arr2 = new OrdArray(maxSize);
+
+		arr2.insert(34); 
+		arr2.insert(54); 
+		arr2.insert(23); 
+		arr2.insert(54); 
+		arr2.insert(76); 
+		arr2.insert(87); 
+		arr2.insert(63); 
+		arr2.insert(82); 
+		arr2.insert(45); 
+		arr2.insert(73); 
+		arr2.insert(78); 
+
+		arr2.display();
+
+		//MERGE TWO ARRAY 
+		OrdArray arr3 = OrdArray.merge(arr, arr2, maxSize);
+
+		arr3.display();
+		
+	}
+}	
