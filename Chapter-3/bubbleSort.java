81ec334f38a70618983b67e2dd185c5f45ee0ee4
srandy <sandeepranjan2007@yahoo.co.in>
2018-07-31 20:29:01 +0530

    Selection sort and bubble sort sloved

---
+++ Chapter-3/bubbleSort.java
@@ -0,0 +1,123 @@
+/*
+	Solution for Programming Projects
+	3.1, 3.4
+*/
+
+
+class ArrayBub {
+	private long[] a;
+	private int nElems;
+
+	public ArrayBub(int max) {
+		a = new long[max];
+		nElems = 0;
+	}
+
+	public void insert(long value) {
+		a[nElems] = value;
+		nElems++;
+	}
+
+	public void display() {
+		for(int j=0; j<nElems; j++)
+			System.out.print(a[j] + " "); 
+		System.out.println("");
+	}
+
+	/*
+	In the bubbleSort program, the invariant is that the data items to the right of out are sorted. This remains true throughout the running of the algorithm. (On the first pass, nothing has been sorted yet, and there are no items to the right of out because it starts on the rightmost element.)
+	*/
+
+	public void bubbleSort() {
+		int out, in;
+		for(out=nElems-1; out>1; out--)
+			for(in=0; in<out; in++)
+				if( a[in] > a[in+1] )
+					swap(in, in+1);
+	}
+
+	
+	public void biDirectionBubbleSort() {
+		int out=nElems-1, in, _out=0;
+		
+		boolean isSwap = true;
+		
+		while(isSwap) {
+			isSwap = false;
+			
+			for(in=_out; in<out; in++) {
+				if( a[in] > a[in+1] ) {
+					swap(in, in+1);
+					isSwap=true;
+				}
+			}
+			
+			for(in=out-1; in>_out; in--) {
+				if(a[in] < a[in-1]) {
+					swap(in, in-1);
+					isSwap=true;
+				}
+			}
+			
+			_out++;
+			out--;
+		}
+			
+	}
+	
+	public void oddEvenSort() {
+		boolean isSwap=true;
+		
+		while(isSwap) {
+			isSwap = false;
+			
+			for(int in=1; in<nElems-1; in+=2) {
+				if( a[in] > a[in+1] ) {
+					swap(in, in+1);
+					isSwap=true;
+				}
+			}
+			
+			for(int in=0; in<nElems-1; in+=2) {
+				if( a[in] > a[in+1] ) {
+					swap(in, in+1);
+					isSwap=true;
+				}
+			}
+			
+		}
+		
+	}
+
+	private void swap(int one, int two) {
+		long temp = a[one];
+		a[one] = a[two];
+		a[two] = temp;
+
+	}
+
+}
+
+class bubbleSort {
+	public static void main(String[] args) {
+		int maxSize = 100;
+		ArrayBub arr = new ArrayBub(maxSize);
+
+		arr.insert(77);
+		arr.insert(99);
+		arr.insert(44);
+		arr.insert(55);
+		arr.insert(22);
+		arr.insert(88);
+		arr.insert(11);
+		arr.insert(00);
+		arr.insert(66);
+		arr.insert(33);
+
+		arr.display();
+
+		// arr.oddEvenSort();
+
+		// arr.display();
+	}
+}
---
+++ Chapter-3/insertionSort.java
@@ -0,0 +1,155 @@
+/*
+	Solution for Programming Projects
+	3.2, 3.3
+*/
+
+class ArrayIns {
+	private long[] a;
+	private int nElems;
+
+	public ArrayIns(int max) {
+		a = new long[max];
+		nElems = 0;
+	}
+
+	public void insert(long value) {
+		a[nElems] = value;
+		nElems++;
+	}
+
+	public void display() {
+		for(int j=0; j<nElems; j++)
+			System.out.print(a[j] + " "); 
+		System.out.println("");
+	}
+
+	public void insertionSort() {
+		int in, out;
+
+		for(out=1; out<nElems; out++) {
+			long temp = a[out];
+			in = out;
+			while(in>0 && a[in-1] >= temp) {
+				a[in] = a[in-1];
+				--in;
+			}
+
+			a[in] = temp;
+
+		} 
+	}
+
+	private void swap(int one, int two) {
+		long temp = a[one];
+		a[one] = a[two];
+		a[two] = temp;
+
+	}
+
+	public long median() {
+		insertionSort();
+
+		int n = nElems - 1;
+ 		
+        if (n % 2 != 0)
+        return a[n / 2];
+     
+        return (a[(n - 1) / 2] + a[n / 2]) / 2;
+	}			  
+
+	
+
+	public void noDups() {
+   		insertionSort();
+
+   		int nDel = 0;
+   		long comp = a[0];
+
+   		for (int i=1; i<nElems; i++) {
+
+	   		if (a[i]==comp) {
+	   			a[i]=-1;
+	   			nDel++;
+	   		} else { 
+	   			comp = a[i];
+	   		}
+
+   		}
+
+	   	for (int i=0; i<nElems;i++) {
+
+	   		if (a[i] == -1) {
+	   			int j=i+1;
+	   			
+	   			while(a[j] == -1) {
+	   				j++;
+	   			}
+
+	   			a[i]=a[j];
+	   			a[j]=-1;
+	   		}
+	   	}
+
+	   	nElems -= nDel;
+   }
+
+    public void insertionSort_and_noDups() {
+  	 
+		int in, out;
+		int nDups = 0;
+	
+		for(out=1; out<nElems; out++) {
+			long temp = a[out];
+		  	in = out;
+
+		  	while(in > 0 && a[in-1] >= temp) {
+
+		  		if (a[in-1] == temp) {
+					temp = -1;
+					nDups++;
+				}
+
+				a[in] = a[in-1];
+				--in;
+		  	}
+
+		  a[in] = temp;
+		}
+
+		if (nDups != 0) {
+		   for (int i = nDups; i < nElems; i++) {
+				a[i - nDups] = a [i];
+			}
+		}
+
+		nElems -= nDups;
+   }
+
+}
+
+class insertionSort {
+	public static void main(String[] args) {
+		int maxSize = 100;
+		ArrayIns arr = new ArrayIns(maxSize);
+
+		arr.insert(77);
+		arr.insert(99);
+		arr.insert(44);
+		arr.insert(55);
+		arr.insert(22);
+		arr.insert(88);
+		arr.insert(11);
+		arr.insert(00);
+		arr.insert(33);
+		arr.insert(33);
+
+		// arr.display();
+
+		// arr.insertionSort();
+
+		// System.out.println(arr.median());
+		arr.display();
+		arr.insertionSort_and_noDups();
+
+	}
+}
