541699ec44a51072c471e662f56c925eedbead78
Saurav Jain <jainsaurav52@gmail.com>
2018-10-02 13:40:38 +0530

    create dfs.java

---
+++ Chapter-13/dfs.java
@@ -0,0 +1,61 @@
+import java.io.*; 
+import java.util.*; 
+
+class Graph 
+{ 
+    private int V;   // No. of vertices 
+ 
+    private LinkedList<Integer> adj[]; 
+
+    Graph(int v) 
+    { 
+        V = v; 
+        adj = new LinkedList[v]; 
+        for (int i=0; i<v; ++i) 
+            adj[i] = new LinkedList(); 
+    } 
+
+    void addEdge(int v, int w) 
+    { 
+        adj[v].add(w);  // Add w to v's list. 
+    } 
+    void DFSUtil(int v,boolean visited[]) 
+    { 
+        // Mark the current node as visited and print it 
+        visited[v] = true; 
+        System.out.print(v+" "); 
+        Iterator<Integer> i = adj[v].listIterator(); 
+        while (i.hasNext()) 
+        { 
+            int n = i.next(); 
+            if (!visited[n]) 
+                DFSUtil(n, visited); 
+        } 
+    } 
+    void DFS(int v) 
+    { 
+        // Mark all the vertices as not visited(set as 
+        // false by default in java) 
+        boolean visited[] = new boolean[V]; 
+  
+        // Call the recursive helper function to print DFS traversal 
+        DFSUtil(v, visited); 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        Graph g = new Graph(4); 
+  
+        g.addEdge(0, 1); 
+        g.addEdge(0, 2); 
+        g.addEdge(1, 2); 
+        g.addEdge(2, 0); 
+        g.addEdge(2, 3); 
+        g.addEdge(3, 3); 
+  
+        System.out.println("Following is Depth First Traversal "+ 
+                           "(starting from vertex 2)"); 
+  
+        g.DFS(2); 
+    } 
+} 
