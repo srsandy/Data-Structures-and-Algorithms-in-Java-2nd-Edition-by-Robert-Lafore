d9acbabdca4279c1261302f7a503a2fb41bd797a
srsandy <sandeepranjan2007@yahoo.co.in>
2018-10-03 02:22:19 +0530

    Knapscak added

---
+++ Chapter-6/Knapsack/Knapsack_MultiSolution.java
@@ -0,0 +1,47 @@
+/*
+    Solved By: Ganesh varma (https://github.com/gane5hvarma)
+    Modification: Sandeep Ranjan (1641012352)
+*/
+
+class Knapsack_MultiSolution {
+    public static int length;
+    public static int[] weights;
+
+    public static boolean knapsack(int startPoint,int capacity){
+       if(weights[startPoint]==capacity){
+           System.out.print(weights[startPoint] + " ");
+           return true;
+       }
+        else{
+            if(weights[startPoint]<capacity){
+
+                for(int i=startPoint+1; i<length; i++){   
+                    boolean value = knapsack(i,capacity-weights[startPoint]);
+
+                    if(value){
+                        System.out.print(weights[startPoint] + " ");
+                        return true;
+                        
+                    }
+                }
+                
+            }
+        }
+        
+        return false;   
+    } 
+
+    public static void main(String args[]){
+
+        weights = new int[]{5, 3, 2, 7};
+        length = weights.length;
+
+        int capacity = 10;
+
+        System.out.println("Weights are ->");
+        for(int i=0; i<length; i++){
+            boolean line = knapsack(i,capacity);
+            if(line) System.out.println();
+        }
+    }
+}
---
+++ Chapter-6/Knapsack/Knapsack_OneSolution.java
@@ -0,0 +1,52 @@
+/*
+    Solved By: Ganesh varma (https://github.com/gane5hvarma)
+    Modification: Sandeep Ranjan (1641012352)
+
+    Solution for Programming Projects
+    6.4
+*/
+
+class Knapsack_OneSolution{
+    public static int length;
+    public static int[] weights;
+
+    public static boolean knapsack(int startPoint, int capacity){
+        for(int i=startPoint; i<length; i++){
+        
+           if(weights[i]==capacity){
+               System.out.print(weights[i] + " ");
+               return  true;
+            }
+            else{
+
+                if(weights[i]<capacity){
+
+                    for(int j=i+1; j<length; j++){   
+
+                        boolean value = knapsack(j,capacity-weights[i]);
+
+                        if(value){
+                            System.out.print(weights[i]+ " ");
+                            return true;
+                        }
+                    }
+                    
+                }
+            }
+        }
+
+        return false;
+    } 
+    
+    public static void main(String args[]){
+
+        weights = new int[]{11, 8, 7, 6, 5};
+        length = weights.length;
+
+        int capacity = 20;
+
+        System.out.print("Weights are -> ");
+        knapsack(0,capacity);
+         
+     }
+} 
