60073e23e40994412999ea1a80b2d60479c948c2
srsandy <sandeepranjan2007@yahoo.co.in>
2018-10-03 20:19:21 +0530

    modified RecursiveMultiplication.java

--- Chapter-6/RecursiveMultiplication.java
+++ Chapter-6/RecursiveMultiplication.java
@@ -1,31 +1,20 @@
+/*
+    Solved By: Joshua (https://github.com/monst12)
+    Modification: Sandeep Ranjan (1641012352)
+
+    
+    Solution for Programming Project
+    6.3
+*/
+
 class RecursiveMultiplication {
     public static void main(String[] args) {
-        if(args.length == 0) {
-            final int mult1 = mult(2, 4);
-            final int mult2 = mult(10, 5);
-            final int mult3 = mult(5, 2);
-            final int mult4 = mult(-2, 6);
-
-            System.out.println("2 * 4: " + mult1);
-            System.out.println("10 * 5: " + mult2);
-            System.out.println("5 * 2: " + mult3);
-            System.out.println("-2 * 6: " + mult4);
-        } else if(args.length >= 2) {
-            try {
-                final int x = Integer.parseInt(args[0]);
-                final int y = Integer.parseInt(args[1]);
-
-                final int result = mult(x, y);
-                System.out.println(x + " * " + y + " = " + result);
-            } catch(NumberFormatException exc) {
-                System.out.println("Please provide two integers as arguments!");
-            }
-        }
+        int x = 2, y = 4;
+        System.out.println("2 * 4: " + mult(x, y));
     }
 
-    private static int mult(int x, int y) {
+    public static int mult(int x, int y) {
         if(y == 1) return x;
         return x + mult(x, (y - 1));
     }
-
 }
