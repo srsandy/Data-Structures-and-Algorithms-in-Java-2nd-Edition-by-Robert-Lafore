2c7db97a3076cb67e84625e343c1a605fc6ce59a
srsandy <sandeepranjan2007@yahoo.co.in>
2018-10-03 20:30:20 +0530

    power 0 logic added

--- Chapter-6/NumberPower.java
+++ Chapter-6/NumberPower.java
@@ -7,15 +7,17 @@
 
 class NumberPower {
 	public static void main(String[] args) {
-		int x = 5;
-		int y = 4;
+		int x = 2;
+		int y = 3;
 
 		System.out.println(x + "^" + y + "= " + power(x,y));
 		
 	}
 
 	public static int power(int x, int y) {
-		if (y == 1) {
+		if(y == 0) {
+			return 1;
+		} else if (y == 1) {
       		return x;
     	} else {
       		int a = power(x*x, y / 2);
