1fc49c8e8e4aa9089b3880ce8e4157a3cdf521d4
Saurav Jain <jainsaurav52@gmail.com>
2018-10-04 18:07:33 +0530

    update (6.5).java
    It contains 2 different methods.

--- Chapter-6/(6.5).java
+++ Chapter-6/(6.5).java
@@ -1,5 +1,7 @@
 import java.lang.*;
 import java.util.*;
+
+// Method 1:
 class Combinations{
    
 static void makeCombiUtil(Vector<Integer> tmp, int n, int left, int k) 
@@ -36,3 +38,43 @@ public static void main(String args[])
 	
 } 
 }
+
+//Method 2:
+class Comb { 
+
+    static void combination(int arr[], int data[], int start, 
+                                int end, int index, int r) 
+    { 
+        if (index == r) 
+        { 
+            for (int j=0; j<r; j++) 
+                System.out.print(data[j]+" "); 
+            System.out.println(""); 
+            return; 
+        } 
+        for (int i=start; i<=end && end-i+1 >= r-index; i++) 
+        { 
+            data[index] = arr[i]; 
+            combination(arr, data, i+1, end, index+1, r); 
+        } 
+    } 
+
+    static void print(int arr[], int n, int r) 
+    { 
+        int data[]=new int[r]; 
+        combination(arr, data, 0, n-1, 0, r); 
+    } 
+
+    public static void main (String[] args) { 
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	int i=0,j=0;
+	int arr[]=new int[n];
+	for(i=1;i<=n;i++)
+	{
+	    arr[j++]=i;
+	}
+        int r = s.nextInt(); 
+        print(arr, n, r); 
+    } 
+} 
