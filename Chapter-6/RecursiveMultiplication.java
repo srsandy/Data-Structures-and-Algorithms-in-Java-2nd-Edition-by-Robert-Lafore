7611448aa25b0ba0ae043456ecea4731a62b757c
Sandeep「 Flame 」 <sandeepranjan2007@yahoo.co.in>
2018-10-12 18:54:43 +0530

    Update RecursiveMultiplication.java

--- Chapter-6/RecursiveMultiplication.java
+++ Chapter-6/RecursiveMultiplication.java
@@ -14,6 +14,7 @@ class RecursiveMultiplication {
     }
 
     public static int mult(int x, int y) {
+        if(y == 0) return 0;
         if(y == 1) return x;
         return x + mult(x, (y - 1));
     }
