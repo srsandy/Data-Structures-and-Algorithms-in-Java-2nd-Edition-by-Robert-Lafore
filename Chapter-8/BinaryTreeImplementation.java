83c09ce9adec03914439bb171eff30b072bb41b0
srsandy <sandeepranjan2007@yahoo.co.in>
2018-10-27 01:14:38 +0530

    BST delete()

--- Chapter-8/BinaryTreeImplementation.java
+++ Chapter-8/BinaryTreeImplementation.java
@@ -82,6 +82,91 @@ class BinaryTree{
     // }
 
 
+    public void delete(int key) {
+        if(root == null) {
+            System.out.println("Tree has 0 nodes");
+            return;
+        }
+
+        Node p = root;
+        Node p1 = null;
+
+        while(p != null && p.data != key) {
+            p1 = p;
+
+            if(p.data > key) {
+                p = p.left;
+            } else {
+                p = p.right;
+            }
+        }
+
+        if(p == null) {
+            System.out.println("Key does not exist");
+            return;
+        }
+
+        if(p.left == null && p.right == null) {
+
+            if(p.data > p1.data) {
+                p1.right = null;
+            } else {
+                p1.left = null;
+            }
+
+        } else if(p.left != null && p.right == null) {
+
+            if(p1.data > p.left.data) {
+                p1.left = p.left;
+            } else {
+                p1.right = p.left;
+            }
+
+        } else if(p.right != null && p.left == null) {
+
+            if(p1.data > p.right.data) {
+                p1.right = p.right;
+            } else {
+                p1.right = p.right;
+            }
+
+        } else if(p.right != null && p.left != null ) {
+            Node s = p.right;
+            Node s1 = null;
+
+            if(s.left != null) {
+
+                while(s.left != null) {
+                    s1 = s;
+                    s = s.left;
+                }
+
+                if(s1 != null)
+                    s1.left = null; // to break the link
+
+                if(s.right != null) {
+                    s1.left = s.right;
+                }
+                
+                s.right = p.right;
+            }
+
+            s.left = p.left;
+
+            if(p1 != null) {
+                if(p1.data > s.data) {
+                    p1.left = s;
+                } else {
+                    p1.right = s;
+                }
+            } else {
+                root = s;
+            }
+        } 
+
+    }
+
+
 
     public void inOrderPrint(Node root){
         if(root == null)
@@ -114,24 +199,115 @@ class BinaryTree{
 public class BinaryTreeImplementation{
     public static void main(String[] args){
 
-        int[] nodes = {11,15,18,24,33,8,7,6,9,14,4,1,55,25,17};
-        BinaryST bst = new BinaryST();
+        int[] nodes = {13, 4, 9, 6, 23, 27, 21, 22, 15, 2, 3, 7};
+        BinaryTree bst = new BinaryTree();
 
 
         for(int i=0; i<nodes.length; i++) {
-            bst.addNode(x[i]);
+            bst.addNode(nodes[i]);
             // bst.addNode(new Node(x[i]),bst.getRoot());
         }
 
-        System.out.println(":::::::::::Pre Order Traversal:::::::::::");
+        /*
+                     13
+                   /    \
+                  4       23
+                 / \    /    \
+                3   9  21    27
+               /   /  / \
+              2   6  15  22
+                   \
+                    7
+        */
+
+        System.out.println("::__Pre Order Traversal__::");
         bst.preOrderPrint(bst.getRoot());
 
         System.out.println();
-        System.out.println(":::::::::::In Order Traversal:::::::::::");
+        System.out.println("::__In Order Traversal__::");
         bst.inOrderPrint(bst.getRoot());
 
         System.out.println();
-        System.out.println(":::::::::::Post Order Traversal:::::::::::");
+        System.out.println("::__Post Order Traversal__::");
         bst.postOrderPrint(bst.getRoot());
+        System.out.println();
+
+
+        // Delete 7
+        bst.delete(7);
+
+
+        /*
+                     13
+                   /    \
+                  4       23
+                 / \    /    \
+                3   9  21    27
+               /   /  / \
+              2   6  15  22
+
+        Inorder : 2 3 4 6 9 13 15 21 22 23 27
+        */
+
+        // Delete 3
+        bst.delete(3);
+
+        /*
+                     13
+                   /    \
+                  4       23
+                 / \    /    \
+                2   9  21    27
+                   /  / \
+                  6  15  22
+
+        Inorder : 2 4 6 9 13 15 21 22 23 27
+        */
+
+        // Delete 4
+        bst.delete(4);
+
+        /*
+                     13
+                   /    \
+                  6       23
+                 / \    /    \
+                2   9  21    27
+                      / \
+                     15  22
+
+        Inorder : 2 6 9 13 15 21 22 23 27
+        */
+
+        // Delete 23 
+        bst.delete(23);
+
+        /*
+                     13
+                   /    \
+                  6      27
+                 / \    /   
+                2   9  21   
+                      / \
+                     15  22
+
+        Inorder : 2 6 9 13 15 21 22 27
+        */
+
+        // Delete 13 
+        bst.delete(13);
+
+        /*
+                     15
+                   /    \
+                  6      27
+                 / \    /   
+                2   9  21   
+                        \
+                        22
+
+        Inorder : 2 6 9 15 21 22 27
+        */
+
     }
 }
