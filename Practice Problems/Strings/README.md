c0dab8c074dffb3d2e1e87e37476e6aad701cb8d
srsandy <sandeepranjan2007@yahoo.co.in>
2019-08-07 12:20:36 +0530

    --added: Strings ValidPalindrome.java

--- Practice Problems/Strings/README.md
+++ Practice Problems/Strings/README.md
@@ -3,4 +3,5 @@ All questions added here are taken from various sites like GeeksForGeeks, LeetCo
 
 ## Problems
 - [Hamming Distance](https://github.com/srsandy/Data-Structures-and-Algorithms-in-Java-2nd-Edition-by-Robert-Lafore/tree/master/Practice%20Problems/Strings/Hamming%20Distance)
+- [Valid Palidrome](https://github.com/srsandy/Data-Structures-and-Algorithms-in-Java-2nd-Edition-by-Robert-Lafore/tree/master/Practice%20Problems/Strings/Valid%20Palidrome)
 
---
+++ Practice Problems/Strings/Valid Palindrome/ValidPalindrome.java
@@ -0,0 +1,34 @@
+class ValidPalindrome {
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while(left < right) {
+            while(!Character.isLetterOrDigit(s.charAt(left)) && left < right) {
+                left++;
+            }
+
+            while(!Character.isLetterOrDigit(s.charAt(right)) && right > left) {
+                right--;
+            }
+            
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+
+    public static void main(String[] args) {
+        
+        String str1 = "Red rum, sir, is murder"; 
+
+        boolean is = isPalindrome(str1);
+
+        System.out.println(is);
+    }
+}
