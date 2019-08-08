231ed5f4b2fe3ed04f284a83a92451d00af101c4
srsandy <sandeepranjan2007@yahoo.co.in>
2019-08-08 11:40:27 +0530

    --added: Arrays NonDecreasingArray.java

---
+++ Practice Problems/Arrays/Non Decreasing Array/NonDecreasingArray.java
@@ -0,0 +1,31 @@
+public NonDecreasingArray {
+	public static void main(String[] args) {
+
+		int[] nums = {-1, 4, 2, 3};
+
+		System.out.println(checkPossibility(nums))
+	}
+
+	public static boolean checkPossibility(int[] nums) {
+        
+        int n = nums.length;
+        int prev = nums[0];
+        boolean c = false;
+        
+        for(int i=1; i<n; i++) {
+            if(nums[i] >= prev) {
+                prev = nums[i];
+                continue;
+            }
+            
+            if(c) return false;
+            c = true;
+            
+            if (i == 1 || nums[i] >= nums[i - 2]) {
+                 prev = nums[i];
+            }
+        }
+        
+        return true;
+    }
+}
--- Practice Problems/Arrays/README.md
+++ Practice Problems/Arrays/README.md
@@ -3,4 +3,5 @@ All questions added here are taken from various sites like GeeksForGeeks, LeetCo
 
 ## Problems
 - [Find All Numbers Disappeared In An Array](https://github.com/srsandy/Data-Structures-and-Algorithms-in-Java-2nd-Edition-by-Robert-Lafore/tree/master/Practice%20Problems/Arrays/Find%20All%20Numbers%20Disappeared%20In%20An%20Array)
+- [Non Decreasing Array](https://github.com/srsandy/Data-Structures-and-Algorithms-in-Java-2nd-Edition-by-Robert-Lafore/tree/master/Practice%20Problems/Arrays/Non%20Decreasing%20Array)
 - [Sock Merchant](https://github.com/srsandy/Data-Structures-and-Algorithms-in-Java-2nd-Edition-by-Robert-Lafore/tree/master/Practice%20Problems/Arrays/Sock%20Merchant)
