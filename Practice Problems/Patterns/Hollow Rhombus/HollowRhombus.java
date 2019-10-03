ed4ae70b9f29875eae087025fee4b8666279e4e1
Pranshu Aggarwal <40149082+pranshuag9@users.noreply.github.com>
2019-10-03 23:48:16 +0530

    Update HollowRhombus Code

--- Practice Problems/Patterns/Hollow Rhombus/HollowRhombus.java
+++ Practice Problems/Patterns/Hollow Rhombus/HollowRhombus.java
@@ -1,34 +1,16 @@
-import java.util.*;
-
-class HollowRhombus {
-    public static void printPattern(int rows) {
-
-
-        for(int i = 0; i<rows; i++) {
-            int j;    
-
-            for(j = rows; j> i; j--) { 
-                System.out.print(" ");
-            }
-
-            for(j=0; j<rows; j++) {
-            
-                if(i==0 || i==rows-1 || j==0 || j==rows-1)
-                    System.out.print("* ");
-                else
-                    System.out.print("  ");
-            }
-            
-            System.out.println();
-
-        }
-    }
-
+import java.util.Scanner;
+public class HollowRhombus {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-
-        printPattern(n);
+        int n=sc.nextInt();
+        for(int row=1,first=n,last=(2*n)-1;row<=n;row++,first--,last--){
+            for(int col=1;col<=last;col++){
+                if(col < first) System.out.print("  ");
+                else if (row == 1 || row == n) System.out.print("* ");
+                else if (col == first || col == last) System.out.print("* ");
+                else System.out.print("  ");
+            }
+            System.out.println();
+        }
     }
 }
