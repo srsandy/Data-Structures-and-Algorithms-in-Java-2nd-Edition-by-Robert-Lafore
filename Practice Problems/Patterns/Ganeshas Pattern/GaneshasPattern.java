3fa39927f946c55fb44687ea7a8ea6fa55448115
Pranshu Aggarwal <pranshuaggarwal99@gmail.com>
2019-10-04 10:29:31 +0530

    Added GaneshasPattern Problem

---
+++ Practice Problems/Patterns/Ganeshas Pattern/GaneshasPattern.java
@@ -0,0 +1,32 @@
+package Challenge1__FundamentalsAndPatterns;
+
+import java.util.Scanner;
+public class GaneshasPattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        printGaneshasPattern(n);
+    }
+    private static void printGaneshasPattern(int n){
+        if(n%2==1){
+            int first=1,middle=(n+1)/2,last=n;
+            for(int row=1;row<=n;row++){
+                for(int col=1;col<=n;col++){
+                    if(row<middle){
+                        if(col<middle) if (col == first) System.out.print("*");
+                        else System.out.print(" ");
+                        if(col==middle) System.out.print("*");
+                        if(col>middle) if (row == first) System.out.print("*");
+                    }else if(row==middle) System.out.print("*");
+                    else if (col < middle) if (row == last) System.out.print("*");
+                    else System.out.print(" ");
+                    else if (col == middle) System.out.print("*");
+                    else if (col == last) System.out.print("*");
+                    else System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
--- Practice Problems/Patterns/README.md
+++ Practice Problems/Patterns/README.md
@@ -3,4 +3,5 @@ All questions added here are taken from various sites like GeeksForGeeks, LeetCo
 
 ## Problems
 - [Diamond](https://github.com/srsandy/Data-Structures-and-Algorithms-in-Java-2nd-Edition-by-Robert-Lafore/tree/master/Practice%20Problems/Patterns/Diamond)
+- [Ganeshas Pattern](https://github.com/srsandy/Data-Structures-and-Algorithms-in-Java-2nd-Edition-by-Robert-Lafore/tree/master/Practice%20Problems/Patterns/Ganeshas%20Pattern)
 - [Hollow Rhombus](https://github.com/srsandy/Data-Structures-and-Algorithms-in-Java-2nd-Edition-by-Robert-Lafore/tree/master/Practice%20Problems/Patterns/Hollow%20Rhombus)
