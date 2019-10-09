fe5260ac2a81745584fd3759669122c2e37ae1e9
srsandy <sandeepranjan2007@yahoo.co.in>
2019-10-09 18:04:52 +0530

    --modified: GaneshasPatter.java

--- Practice Problems/Patterns/Ganeshas Pattern/GaneshasPattern.java
+++ Practice Problems/Patterns/Ganeshas Pattern/GaneshasPattern.java
@@ -7,23 +7,42 @@ public class GaneshasPattern {
     }
     private static void printGaneshasPattern(int n){
         if(n%2==1){
-            int first=1,middle=(n+1)/2,last=n;
-            for(int row=1;row<=n;row++){
-                for(int col=1;col<=n;col++){
+            int first=1, middle=(n+1)/2, last=n;
+
+            for(int row=1; row<=n; row++){
+                for(int col=1; col<=n; col++){
                     if(row<middle){
-                        if(col<middle) if (col == first) System.out.print("*");
-                        else System.out.print(" ");
-                        if(col==middle) System.out.print("*");
-                        if(col>middle) if (row == first) System.out.print("*");
-                    }else if(row==middle) System.out.print("*");
-                    else if (col < middle) if (row == last) System.out.print("*");
-                    else System.out.print(" ");
-                    else if (col == middle) System.out.print("*");
-                    else if (col == last) System.out.print("*");
-                    else System.out.print(" ");
+                        if(col<middle) 
+                            if (col == first) 
+                                System.out.print("*");
+                        else 
+                            System.out.print(" ");
+
+                        if(col==middle) 
+                            System.out.print("*");
+                        if(col>middle) 
+                            if (row == first) 
+                                System.out.print("*");
+
+                    } else if(row==middle) 
+                        System.out.print("*");
+                    else if (col < middle) 
+                        if (row == last) 
+                            System.out.print("*");
+                        else 
+                            System.out.print(" ");
+
+                    else if (col == middle) 
+                        System.out.print("*");
+                    else if (col == last) 
+                        System.out.print("*");
+                    else 
+                        System.out.print(" ");
                 }
                 System.out.println();
             }
+        } else {
+            System.out.println("Enter odd digit");
         }
     }
 }
